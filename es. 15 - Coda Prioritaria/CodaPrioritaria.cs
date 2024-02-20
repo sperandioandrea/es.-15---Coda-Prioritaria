6c72411665bd0b9761c4834cb1326fb5d4d3ff5e
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2024-02-20 13:18:50 +0100

    Ricerca Elemento

--- es. 15 - Coda Prioritaria/CodaPrioritaria.cs
+++ es. 15 - Coda Prioritaria/CodaPrioritaria.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace es._15___Coda_Prioritaria
 {
-    internal class CodaPrioritaria
+    public class CodaPrioritaria
     {
+        private ArrayList lista = new ArrayList();
+
+
+        //Inserimento Elemento 
+        public void Aggiunta(object elemento)
+        {
+            if (lista.Count == 0)
+            {
+                lista.Add(elemento);
+            }
+            else
+            {
+                int indiceInserimento = 0;
+                while (indiceInserimento < lista.Count && ((IComparable)lista[indiceInserimento]).CompareTo(elemento) < 0)
+                {
+                    indiceInserimento++;
+                }
+                lista.Insert(indiceInserimento, elemento);
+            }
+        }
+
+        //Ricerca Elemento
+        public bool Ricerca(object elemento)
+        {
+            return lista.Contains(elemento);
+        }
+
+        
+
     }
 }
